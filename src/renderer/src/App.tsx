66582a98a46832170750471966e7654df0345bae
wll_cyg@126.com <wanliang123>
2024-03-20 14:30:31 +0800

    init

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,25 @@
+import Versions from './components/Versions'
+
+function App(): JSX.Element {
+  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+
+  return (
+    <>
+      <div className="actions">
+        <div className="action">
+          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
+            Documentation
+          </a>
+        </div>
+        <div className="action">
+          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
+            Send IPC
+          </a>
+        </div>
+      </div>
+      <Versions></Versions>
+    </>
+  )
+}
+
+export default App
